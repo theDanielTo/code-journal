c52a378282090cd26d3e1eb64c881629c2bcb091
theDanielTo <dto1989@gmail.com>
2021-05-24 14:49:45 -0700

    I1: journal entries saved into local storage

--- js/data.js
+++ js/data.js
@@ -6,3 +6,11 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+const prevDataJSON = window.localStorage.getItem('journal-local-storage');
+if (prevDataJSON !== null) data = JSON.parse(prevDataJSON);
+
+window.addEventListener('beforeunload', function (event) {
+  const dataJSON = JSON.stringify(data);
+  window.localStorage.setItem('journal-local-storage', dataJSON);
+});
--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
 /* global data */
 /* exported data */
+// https://www.bbvaopenmind.com/wp-content/uploads/2015/12/Ada_Lovelace_Chalon_portrait-1-1024x1024-1.jpg
+
+const entryForm = document.querySelector('form');
+const formImg = document.querySelector('#form-image');
+const titleInput = document.querySelector('#title-input');
+const photoUrlInput = document.querySelector('#entry-photo-url');
+const notesTextArea = document.querySelector('#new-entry-notes');
+
+photoUrlInput.addEventListener('input', function (event) {
+  formImg.src = event.target.value;
+});
+
+entryForm.addEventListener('submit', function (event) {
+  const newEntry = {
+    title: '',
+    imgUrl: '',
+    notes: '',
+    id: 0
+  };
+
+  newEntry.title = titleInput.value;
+  newEntry.imgUrl = photoUrlInput.value;
+  newEntry.notes = notesTextArea.value;
+  newEntry.id = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(newEntry);
+  event.preventDefault();
+  formImg.src = 'images/placeholder-image-square.jpg';
+  titleInput.value = '';
+  photoUrlInput.value = '';
+  notesTextArea.value = '';
+});
